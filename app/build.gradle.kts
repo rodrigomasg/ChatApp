5d29e46b728d0caefdc9ae9699da154aab44b49b
rodrigomasg <rodrigorodgms@gmail.com>
2024-03-04 16:46:42 -0600

    Login2Fragment send op

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,12 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -61,10 +67,6 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
 
     val nav_version = "2.7.7"
-
-    // Java language implementation
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")

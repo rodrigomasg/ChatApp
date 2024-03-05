68d40d46f556104625d8920b1bb197b4234d5614
rodrigomasg <rodrigorodgms@gmail.com>
2024-03-05 17:28:04 -0600

    view search

--- app/src/main/java/com/azteca/chatapp/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/azteca/chatapp/ui/fragment/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.azteca.chatapp.databinding.FragmentSearchBinding
 
@@ -19,4 +20,29 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        binding.searchIvBack.setOnClickListener { parentFragmentManager.popBackStack() }
+        binding.searchSearchView.setOnQueryTextListener(searchUser)
+    }
+
+    private val searchUser = object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+        override fun onQueryTextChange(newText: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            if (!query.isNullOrEmpty()) searchQuery(query)
+            return true
+        }
+    }
+
+    private fun searchQuery(query: String) {
+
+    }
 }
